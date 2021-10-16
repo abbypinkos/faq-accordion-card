7bade1575c22cf5ca650b3df98f3c669c1b0361b
abbypinkos <abbypinkos@gmail.com>
2021-10-15 21:36:26 -0500

    Add mouseover/mouseout event to buttons

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-document.querySelectorAll('.faq__question').forEach(button => {
+document.querySelectorAll('.faq__text-question').forEach(button => {
     button.addEventListener('click', () => {
         const faq__answer = button.nextElementSibling
 
-        button.classList.toggle('faq__question--active')
+        button.classList.toggle('faq__text-question--active')
 
-        if (button.classList.contains('faq__question--active')) {
-            faq__answer.classList.toggle('faq__answer--active')
+        if (button.classList.contains('faq__text-question--active')) {
+            faq__answer.classList.toggle('faq__text-answer--active')
         } else {
-            faq__answer.classList.toggle('faq__answer--active')
+            faq__answer.classList.toggle('faq__text-answer--active')
         }
+    })
+
+    const block = document.querySelector('.faq__image-desktop-block')
+
+    button.addEventListener('mouseover', () => {
+        block.classList.remove('faq__image-desktop-block--inactive')
+        block.classList.add('faq__image-desktop-block--active')
+    })
+
+    button.addEventListener('mouseout', () => {
+        block.classList.remove('faq__image-desktop-block--active')
+        block.classList.add('faq__image-desktop-block--inactive')
     })
 })
